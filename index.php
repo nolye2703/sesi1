d6a49cba5702a10d9b69076e8998b9e9bf8149c4
nolye2703 <127837344+nolye2703@users.noreply.github.com>
2023-03-14 17:02:48 +0800

    Pemrograman Web_Sesi1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+  <h3>pemrograman web <?="kelas F"?></h3>  
+  
+        <script>
+            document.write("<h3>hari selasa</h3>");
+        </script>
+
+    <?php
+        echo "<h3>jam 250</h3>";
+    ?>
+
+</body>
+</html>
